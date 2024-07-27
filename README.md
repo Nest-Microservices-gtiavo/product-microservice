0a2ce734fad3850bc4a8a48a7422c20a8002228c
gtiavo <victorg.avila@gmail.com>
2024-07-27 11:52:40 -0300

    actualizacion para generar el launch

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 ## Dev
 
 1. Clonar el repositorio
-2. Instalar dependecias
+2. Instalar dependencias
 3. Crear un archivo `.env` basado en el `env.template`
-4. Ejecutar migración de prisma `npx prisma migrate dev`
-5. Ejecutar `npm run start:dev`
+4. Levantar el servidor de NATS
+
+```
+docker run -d --name nats-server -p 4222:4222 -p 8222:8222 nats
+```
+
+5. Tener levantados los microservicios que se van a consumir
+6. Leventar proyecto con `npm run start:dev`
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -48,7 +44,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product)
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        message: `Product with id #${id} not found`,
+        status: HttpStatus.BAD_REQUEST,
+      });
 
     return product;
   }
@@ -77,5 +76,26 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     return product;
+  }
+
+  async validateProducts(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products were no found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+    }
+
+    return products;
   }
 }
